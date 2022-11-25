9310151fde2a9dc502c5dc0094ce658bc6b5518e
Mike van Oorschot <michael.oorschot@codurance.com>
2022-11-25 16:44:52 +0000

    Code comment and README file changes.

--- README.md
+++ README.md
@@ -1,6 +1,22 @@
 # Compare HTTP Versions
 Sample projects to compare/contrast HTTP v1.1 and v2 and v3 in ASP.NET Core
 
-## Links
+## Browsers
+- At the time of writing, Chrome does not allow HTTP/3 with a self-signed certificate. 
+- So use Firefox instead when running this code locally.
+  - It complains about the self-signed certificate but just click "Advanced.." and "Allow...".
+- Use the F12 browser tools "Network" tab to view the protocol used to server the content.
+- You may have to right-click the column headers to select and show the "Protocol" column.
+- Note the following: 
+  - HTTP/3 is discovered as an upgrade from HTTP/1.1 or HTTP/2 via the alt-svc header. That means the first request will normally use HTTP/1.1 or HTTP/2 before switching to HTTP/3.
+  - Also, doing a CTRL+F5 in the browser will restart this process.
+
+## Links serving an image over different HTTP versions
+- HTTP/1.1 http://localhost:5001/
+- HTTPS/1.1 https://localhost:5002/
+- HTTP/2 https://localhost:5003/
+- HTTP/3 https://localhost:5004/
+
+## Doc Links
 - https://learn.microsoft.com/en-us/aspnet/core/fundamentals/servers/kestrel/http2
 - https://learn.microsoft.com/en-us/aspnet/core/fundamentals/servers/kestrel/http3
--- WebResource/Program.cs
+++ WebResource/Program.cs
@@ -5,27 +5,27 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.WebHost.ConfigureKestrel((context, options) =>
 {
-    // HTTP 1.1
+    // HTTP/1.1 on port 5001
     options.ListenAnyIP(5001, listenOptions =>
     {
         listenOptions.Protocols = HttpProtocols.Http1;
     });
     
-    // HTTPS 1.1
+    // HTTPS/1.1 on port 5002
     options.ListenAnyIP(5002, listenOptions =>
     {
         listenOptions.Protocols = HttpProtocols.Http1;
         listenOptions.UseHttps();
     });
     
-    // HTTPS 2
+    // HTTP/2 on port 5003
     options.ListenAnyIP(5003, listenOptions =>
     {
         listenOptions.Protocols = HttpProtocols.Http1AndHttp2;
         listenOptions.UseHttps();
     });
     
-    // HTTPS 3
+    // HTTP/3 on port 5004
     options.ListenAnyIP(5004, listenOptions =>
     {
         listenOptions.Protocols = HttpProtocols.Http1AndHttp2AndHttp3;
@@ -40,7 +40,7 @@ app.MapGet("/", (context) =>
     context.Response.Headers[HeaderNames.CacheControl] = "no-store";
     return Results.File(
             path: Path.Combine(app.Environment.ContentRootPath, @"images/jimmi.jpg"), 
-            contentType: "image.jpeg")
+            contentType: "image/jpeg")
         .ExecuteAsync(context);
 });
 
